fe57db6dbce1dd303e2da68e88aee650819fceb2
Rafiya1207 <rafiya1207@gmail.com>
2025-01-15 12:15:07 +0530

    Refactor TCForm styles and structure; update class names for consistency

--- frontend/src/components/TCForm.jsx
+++ frontend/src/components/TCForm.jsx
@@ -4,8 +4,8 @@ import { TextField, Button, Radio, RadioGroup, FormControlLabel, FormLabel } fro
 
 const TCForm = () => {
 	return (
-		<form>
-			<h1 className="form-heading">Fill Transfer Certificate Form</h1>
+		<form className="tc-form">
+			<h1 className="tc-form-heading">Fill Transfer Certificate Form</h1>
 			<TextField
 				label="Certificate Number"
 				name="certificateNumber"
@@ -54,7 +54,7 @@ const TCForm = () => {
 				margin="normal"
 				sx={{ width: "600px" }}
 				type="date"
-				InputLabelProps={{ shrink: true }}				
+				InputLabelProps={{ shrink: true }}
 			/>
 			<TextField
 				label="Date of Admission"
@@ -62,7 +62,7 @@ const TCForm = () => {
 				margin="normal"
 				sx={{ width: "600px" }}
 				type="date"
-				InputLabelProps={{ shrink: true }}				
+				InputLabelProps={{ shrink: true }}
 			/>
 			<TextField
 				label="Date of Leaving"
@@ -98,35 +98,35 @@ const TCForm = () => {
 				margin="normal"
 				sx={{ width: "600px" }}
 			/>
+			<div className="tc-form-radios">
+				<FormLabel component="legend">Are you promoted to higher class?</FormLabel>
+				<RadioGroup
+					row
+					name="promoted"
+				>
+					<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+					<FormControlLabel value="No" control={<Radio />} label="No" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+				</RadioGroup>
 
-			<FormLabel component="legend">Are You Promoted?</FormLabel>
-			<RadioGroup
-				row
-				name="promoted"
-			>
-				<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{color: "rgba(0, 0, 0, 0.6)"}} />
-				<FormControlLabel value="No" control={<Radio />} label="No" sx={{color: "rgba(0, 0, 0, 0.6)"}}/>
-			</RadioGroup>
+				<FormLabel component="legend">Have you paid all college fee?</FormLabel>
+				<RadioGroup
+					row
+					name="feePaid"
+				>
+					<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+					<FormControlLabel value="No" control={<Radio />} label="No" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+				</RadioGroup>
 
-			<FormLabel component="legend">Have you paid fee?</FormLabel>
-			<RadioGroup
-				row
-				name="feePaid"
-			>
-				<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{color: "rgba(0, 0, 0, 0.6)"}} />
-				<FormControlLabel value="No" control={<Radio />} label="No" sx={{color: "rgba(0, 0, 0, 0.6)"}}/>
-			</RadioGroup>
-
-			<FormLabel component="legend">Applied for Sumoto?</FormLabel>
-			<RadioGroup
-				row
-				name="appliedSumoto"
-			>
-				<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{color: "rgba(0, 0, 0, 0.6)"}} />
-				<FormControlLabel value="No" control={<Radio />} label="No" sx={{color: "rgba(0, 0, 0, 0.6)"}}/>
-			</RadioGroup>
-
-			<Button type="submit" variant="contained" color="primary">
+				<FormLabel component="legend">Whether you have applied for this certificate or it is being given Sumoto?</FormLabel>
+				<RadioGroup
+					row
+					name="appliedSumoto"
+				>
+					<FormControlLabel value="Yes" control={<Radio />} label="Yes" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+					<FormControlLabel value="No" control={<Radio />} label="No" sx={{ color: "rgba(0, 0, 0, 0.6)" }} />
+				</RadioGroup>
+			</div>
+			<Button type="submit" variant="contained" color="primary" className="tc-form-btn">
 				Submit
 			</Button>
 		</form>
