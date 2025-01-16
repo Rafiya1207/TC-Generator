d38ba3240628ad712d81c0bfa9c5e16aebc94f48
Rafiya1207 <rafiya1207@gmail.com>
2025-01-16 10:35:01 +0530

    Add custom hook useUserData for managing user data state

---
+++ frontend/src/hooks/useUserData.js
@@ -0,0 +1,31 @@
+import { useState, useCallback } from "react";
+
+const useUserData = () => {
+  const [userData, setUserData] = useState({
+    certificateNumber: '',
+    admissionNumber: '',
+    fullname: '',
+    fatherName: '',
+    nationality: '',
+    religion: '',
+    caste: '',
+    dob: '',
+    doa: '',
+    dol: '',
+    doiTC: '',
+    yearSem: '',
+    course: '',
+    pin: '',
+  });
+
+  const addUserData = useCallback((event) => {
+    setUserData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value
+    }));
+  }, []);
+
+  return [userData, addUserData];
+};
+
+export default useUserData;
