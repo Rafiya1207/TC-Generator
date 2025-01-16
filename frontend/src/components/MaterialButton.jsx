21bd322f06b115c207a3fab24345d71d1c67379a
Rafiya1207 <rafiya1207@gmail.com>
2025-01-16 15:21:46 +0530

    Enhance MaterialButton component to support submit type and conditional linking

--- frontend/src/components/MaterialButton.jsx
+++ frontend/src/components/MaterialButton.jsx
@@ -2,16 +2,21 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import '../App.css';
 
-const MaterialButton = ({ path, label, className }) => {
-  return (
-    <div className={className}>
-      <Link to={path}>
-        <Button type="button" variant="contained" color="primary">
-          {label}
-        </Button>
-      </Link>
-    </div>
-  );
+const MaterialButton = ({ path, label, type = 'button', className }) => {
+	const button = <Button type={type} variant="contained" color="primary">
+		{label}
+	</Button>
+	return (
+		<div className={className}>
+			{
+				type !== 'submit' ?
+					<Link to={path}>
+						{button}
+					</Link> :
+					{ button }
+			}
+		</div>
+	);
 };
 
 export default MaterialButton;
