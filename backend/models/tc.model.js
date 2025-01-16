583e0b2c0fb6462bbf00b5a216f767f6a18ed449
Rafiya1207 <rafiya1207@gmail.com>
2025-01-16 16:04:51 +0530

    Add TC model schema for student transfer certificates

---
+++ backend/models/tc.model.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose";
+
+const tcSchema = mongoose.Schema({
+	certificateNumber: {
+		type: String,
+		required: true
+	},
+	admissionNumber: {
+		type: String,
+		required: true
+	},
+	fullname: {
+		type: String,
+		required: true
+	},
+	fatherName: {
+		type: String,
+		required: true
+	},
+	nationality: {
+		type: String,
+		required: true
+	},
+	religion: {
+		type: String,
+		required: true
+	},
+	caste: {
+		type: String,
+		required: true
+	},
+	dob: {
+		type: String,
+		required: true
+	},
+	doa: {
+		type: String,
+		required: true
+	},
+	dol: {
+		type: String,
+		required: true
+	},
+	doiTC: {
+		type: String,
+		required: true
+	},
+	yearSem: {
+		type: String,
+		required: true
+	},
+	course: {
+		type: String,
+		required: true
+	},
+	pin: {
+		type: String,
+		required: true
+	},
+	isPromoted: {
+		type: String,
+		required: true
+	},
+	isPaidFee: {
+		type: String,
+		required: true
+	},
+	isApplied: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+}, {
+	timestamps: true
+});
+
+const TC = mongoose.model('TC', tcSchema);
+
+export default TC;
