989cc29ad0473769e62c11e0b7d0b9d5a5e3d762
Rafiya1207 <rafiya1207@gmail.com>
2025-01-16 10:48:10 +0530

    Implement routing for TCForm and Preview components using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import TCForm from './components/TCForm';
+import Preview from './components/Preview';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			<TCForm/>
+			<Routes>
+				<Route path='/' element={<TCForm/>} />
+				<Route path='/preview' element={<Preview/>} />
+			</Routes>
 		</>
 	)
 }
