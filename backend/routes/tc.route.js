132f902fe56113be1df480a5bdd0971885df1ece
Rafiya1207 <rafiya1207@gmail.com>
2025-01-17 15:54:46 +0530

    Add validation for TC creation and add CredentialsForm submission logic

--- backend/routes/tc.route.js
+++ backend/routes/tc.route.js
@@ -1,10 +1,15 @@
-import TC from "../models/tc.model";
+import TC from "../models/tc.model.js";
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
 	const tc = req.body;
+	console.log(tc);
+	
+	if (!tc.certificateNumber && !tc.admissionNumber && !tc.fullname && !tc.fatherName && !tc.nationality && !tc.religion && !tc.caste && !tc.dob && !tc.doa && !tc.dol && !tc.doiTC && !tc.yearSem && !tc.course && !tc.pin && !tc.isPromoted && !tc.isPaidFee && !tc.isApplied && !tc.email && !tc.password) {
+		return res.status(400).json({ success: false, message: "provide fields" });
+	}
 
 	const newTC = new TC(tc);
 	try {
--- frontend/src/components/CredentialsForm.jsx
+++ frontend/src/components/CredentialsForm.jsx
@@ -5,33 +5,49 @@ import linkToPath from "../functions/linkToPath.js";
 import { UserDataContext } from "../context/UserDataContext.jsx";
 
 const CredentialsForm = ({ navigate }) => {
-  const { userData, addUserData } = useContext(UserDataContext);
+	const { userData, addUserData } = useContext(UserDataContext);
 	const { isSubmitted, setIsSubmitted } = useState(false);
 
 	useEffect(() => {
-		
-	})
+		const postTC = async () => {
+			const res = await fetch('http://localhost:5000/tc', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				credentials: 'include',
+				body: JSON.stringify(userData)
+			})
+		};
 
-  return (
-    <form className="tc-form" onSubmit={(event) => linkToPath(event, '/credentials', navigate)}>
-      <h1 className="tc-form-heading">Add Credentials</h1>
-      <MaterialTextField
-        label="Email"
-        name="email"
-        value={userData.email}
-        addUserData={addUserData}
-        type='email'
-      />
-      <MaterialTextField
-        label="Password"
-        name="password"
-        value={userData.password}
-        addUserData={addUserData}
-        type='password'
-      />
-      <MaterialButton type='submit' label='Submit' className='align-right' />
-    </form>
-  );
+		postTC();
+	}, [isSubmitted])
+
+	return (
+		<form
+			className="tc-form"
+			onSubmit={(event) => {
+				// linkToPath(event, '/credentials', navigate)
+				setIsSubmitted(!isSubmitted)
+			}}>
+			<h1 className="tc-form-heading">Add Credentials</h1>
+			<MaterialTextField
+				label="Email"
+				name="email"
+				value={userData.email}
+				addUserData={addUserData}
+				type='email'
+			/>
+			<MaterialTextField
+				label="Password"
+				name="password"
+				value={userData.password}
+				addUserData={addUserData}
+				type='password'
+			/>
+			<MaterialButton type='submit' label='Submit' className='align-right' />
+		</form>
+	);
 };
 
 export default CredentialsForm;
