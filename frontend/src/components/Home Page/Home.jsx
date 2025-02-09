b99dc4014f5ec72f11673ae1f38a64aa5df346cb
Rafiya1207 <rafiya1207@gmail.com>
2025-02-09 16:10:38 +0530

    create Home Page component

---
+++ frontend/src/components/Home Page/Home.jsx
@@ -0,0 +1,14 @@
+import Header from './Header.jsx';
+import MainSection from './MainSection.jsx';
+import Footer from './Footer.jsx'
+const Home = () => {
+	return (
+		<>
+			<Header/>
+			<MainSection/>
+			<Footer/>
+		</>
+	);
+};
+
+export default Home;
