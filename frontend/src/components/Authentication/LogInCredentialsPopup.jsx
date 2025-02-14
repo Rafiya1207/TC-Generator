0264f5c185ad876d15efb2b678f6d22ddaa2bdb0
Rafiya1207 <rafiya1207@gmail.com>
2025-02-14 21:36:55 +0530

    Add password state management to LogInCredentialsPopup component

--- frontend/src/components/Authentication/LogInCredentialsPopup.jsx
+++ frontend/src/components/Authentication/LogInCredentialsPopup.jsx
@@ -1,8 +1,11 @@
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
 import MaterialTextField from '../Material Form Elements/MaterialTextField.jsx';
+import { useState } from "react";
 
 const LogInCredentialsPopup = ({isClickedLogin, close}) => {
-	
+	const [userPassword, setUserPassword] = useState('');
+	const changePassword = (e) => setUserPassword(e.target.value);
+
 	return (
 		<Dialog open={isClickedLogin} onClose={close}>
 			<DialogTitle>Log In</DialogTitle>
@@ -10,7 +13,9 @@ const LogInCredentialsPopup = ({isClickedLogin, close}) => {
 			<MaterialTextField
 					label="Password"
 					name="password"
-					type='password'
+					type="password"
+					value={userPassword}
+					listener={changePassword}
 				/>
 			</DialogContent>
 			<DialogActions>
