cdd650c7ef3960b08b8ac71cc0569bec0649faa0
Rafiya1207 <rafiya1207@gmail.com>
2025-02-15 19:41:00 +0530

    Add dynamic dialog title to LogInCredentialsPopup for better user feedback on login attempts

--- frontend/src/components/Authentication/LogInCredentialsPopup.jsx
+++ frontend/src/components/Authentication/LogInCredentialsPopup.jsx
@@ -4,18 +4,24 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import isPassword from "../../functions/checkPassword.js";
 
-const LogInCredentialsPopup = ({isClickedLogin, close}) => {
+const LogInCredentialsPopup = ({ isClickedLogin, close }) => {
 	const [userPassword, setUserPassword] = useState('');
 	const changePassword = (e) => setUserPassword(e.target.value);
+	const [dialogTitle, setDialogTitle] = useState(true);
 	const navigate = useNavigate();
 	const verifyPassword = () => {
 		if (isPassword(userPassword)) navigate('/dashboard');
+		setDialogTitle(false);
 	}
 	return (
 		<Dialog open={isClickedLogin} onClose={close}>
-			<DialogTitle>Log In</DialogTitle>
+			{
+				dialogTitle
+					? <DialogTitle>Log In</DialogTitle>
+					: <DialogTitle color="red">Log In Failed, Try Again</DialogTitle>
+			}
 			<DialogContent>
-			<MaterialTextField
+				<MaterialTextField
 					label="Password"
 					name="password"
 					type="password"
